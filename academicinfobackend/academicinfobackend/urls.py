a20e2b6dee351828557ed4c97fb756b4d0f0ee58
Oscar <galoscar05@gmail.com>
2018-11-02 16:12:00 +0200

    login backend implemented

--- academicinfobackend/academicinfobackend/urls.py
+++ academicinfobackend/academicinfobackend/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^api/v1/', include(router.urls)),
 ]
